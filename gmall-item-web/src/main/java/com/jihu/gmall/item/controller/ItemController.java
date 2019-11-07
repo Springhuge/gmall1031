3843c2cae98fcd5d8e756c646a3bdfa635f4abe6
jihu <529581062@qq.com>
2019-11-07 22:27:11 +0800

    添加gmall-item-web工程，商品详情功能 引入thymeleaf模板技术

---
+++ gmall-item-web/src/main/java/com/jihu/gmall/item/controller/ItemController.java
@@ -0,0 +1,28 @@
+package com.jihu.gmall.item.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ItemController {
+
+    @RequestMapping("index")
+    public String index(ModelMap modelMap){
+
+        List<String> list = new ArrayList<>();
+        for(int i = 0;i < 5 ; i++){
+            list.add("循环数据"+i);
+        }
+
+        modelMap.put("list",list);
+        modelMap.put("hello","hello thymeleaf !!");
+
+        modelMap.put("check","1");
+        return "index";
+    }
+}
